6604b0a41b6fb207ce0dfdd88bd0767de78f14e1
Evan Mullins <evanrm@gmail.com>
2025-07-31 13:15:47 -0400

    update php version and fix workflow to update it too

--- bootstrap.php
+++ bootstrap.php
@@ -9,7 +9,7 @@ if ( defined( 'NFD_NEXTSTEPS_MODULE_VERSION' ) ) {
 	return;
 }
 
-define( 'NFD_NEXTSTEPS_MODULE_VERSION', '1.0.0' );
+define( 'NFD_NEXTSTEPS_MODULE_VERSION', '1.0.2' );
 
 if ( function_exists( 'add_action' ) ) {
 	add_action(
