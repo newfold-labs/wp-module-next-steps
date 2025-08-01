bb431840a81183a358af8f68e444843a0df7dda0
Evan Mullins <evanrm@gmail.com>
2025-08-01 15:36:28 -0400

    update build and language files

--- build/next-steps-widget/bundle.asset.php
+++ build/next-steps-widget/bundle.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '7bc4d915aabfae374018');
+<?php return array('dependencies' => array('lodash', 'react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'c6ed2a8db1a7544cafdc');
