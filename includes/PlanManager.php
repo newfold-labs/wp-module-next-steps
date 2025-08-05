6664f3893bd63c0ab971a4eeed0edfb71d6721ea
Evan Mullins <evanrm@gmail.com>
2025-08-05 13:08:06 -0400

    update sitemap task link for ecom plan

--- includes/PlanManager.php
+++ includes/PlanManager.php
@@ -540,7 +540,7 @@ class PlanManager {
 									array(
 										'id'       => 'store_create_sitemap',
 										'title'    => __( 'Create a Custom Sitemap', 'wp-module-next-steps' ),
-										'href'     => 'https://www.bluehost.com/blog/how-to-submit-your-website-to-search-engines/',
+										'href'     => 'https://www.bluehost.com/blog/what-is-a-sitemap-how-it-helps-seo-and-navigation/',
 										'status'   => 'new',
 										'priority' => 3,
 										'source'   => 'wp-module-next-steps',
