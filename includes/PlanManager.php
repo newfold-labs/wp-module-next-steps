b51c429e71b0a56768d935bea15b3e5d6efa4003
Evan Mullins <evanrm@gmail.com>
2025-08-08 10:25:15 -0400

    fix blog nav link PRESS0-3151

--- includes/PlanManager.php
+++ includes/PlanManager.php
@@ -866,7 +866,7 @@ class PlanManager {
 									array(
 										'id'       => 'blog_create_primary_menu',
 										'title'    => __( 'Create a Primary Menu', 'wp-module-next-steps' ),
-										'href'     => '{siteUrl}/wp-admin/site-editor.php?p=%2Fwp_navigation%2F7&canvas=edit',
+										'href'     => '{siteUrl}/wp-admin/site-editor.php?p=/navigation',
 										'status'   => 'new',
 										'priority' => 2,
 										'source'   => 'wp-module-next-steps',
