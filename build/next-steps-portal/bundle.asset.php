56b368ee03bd3e07dc7560f600a6432aec2dd0c6
Evan Mullins <evanrm@gmail.com>
2025-08-08 12:04:23 -0400

    update build files

--- build/next-steps-portal/bundle.asset.php
+++ build/next-steps-portal/bundle.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '5a1b36f2c467190cfbad');
+<?php return array('dependencies' => array('lodash', 'react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '3d5724922fbfda01b304');
