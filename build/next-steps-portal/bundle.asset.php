2c53d4e10ca9bbf149ac69b94e093c9952a18627
Evan Mullins <evanrm@gmail.com>
2025-08-11 11:34:04 -0400

    update build files

--- build/next-steps-portal/bundle.asset.php
+++ build/next-steps-portal/bundle.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '86f4e1518103b02498e0');
+<?php return array('dependencies' => array('lodash', 'react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'c96d02cf58d276038050');
--- build/next-steps-widget/bundle.asset.php
+++ build/next-steps-widget/bundle.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '0b5cdbabe0f4a9ed788c');
+<?php return array('dependencies' => array('lodash', 'react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'd82f60ac48b96f35a272');
