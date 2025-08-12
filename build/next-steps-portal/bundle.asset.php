b7a4889fbb934b9e509336af847ca33e337d4e49
Evan Mullins <evanrm@gmail.com>
2025-08-12 14:50:30 -0400

    tweak progress bar label for a11y

--- build/next-steps-portal/bundle.asset.php
+++ build/next-steps-portal/bundle.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '678461c103ab22334f5b');
+<?php return array('dependencies' => array('lodash', 'react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '594fc13a9cf01d02d99d');
--- build/next-steps-widget/bundle.asset.php
+++ build/next-steps-widget/bundle.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '125499b814d0a18fc1d7');
+<?php return array('dependencies' => array('lodash', 'react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '459df7902e0bc7485727');
