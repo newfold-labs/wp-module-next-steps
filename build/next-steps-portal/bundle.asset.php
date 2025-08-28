b646d2b2184c272fccc274f56bac6e70c5d1aaf2
Carlos Rodriguez Brito <crodriguezbrito@gmail.com>
2025-08-28 15:26:24 +0100

    Update Yoast and Jetpack wireframes

--- build/next-steps-portal/bundle.asset.php
+++ build/next-steps-portal/bundle.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '11266b0b3f40fe84605f');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '80baf265a51c7781b42c');
--- build/next-steps-widget/bundle.asset.php
+++ build/next-steps-widget/bundle.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '13fb58c18fe8b6728308');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'ac9b0c5a7cbaf62a6932');
