684bdd660816be8d196b449e96ffd3be881b8c33
Carlos Rodriguez Brito <crodriguezbrito@gmail.com>
2025-08-29 09:52:29 +0100

    Include button link on Done steps

--- build/next-steps-portal/bundle.asset.php
+++ build/next-steps-portal/bundle.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'a2151b189744fa11ea5d');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '8b3e0a92859528187ac4');
--- build/next-steps-widget/bundle.asset.php
+++ build/next-steps-widget/bundle.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '95a16a54d695d1f05774');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'a5d1146883683b8ba01f');
