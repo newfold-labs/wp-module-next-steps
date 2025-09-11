c76c38a90f7f96bdf6ec0a81d8c658f65dade50f
Evan Mullins <evanrm@gmail.com>
2025-09-11 17:34:25 -0400

    add fall-through comment

--- includes/PlanFactory.php
+++ includes/PlanFactory.php
@@ -257,22 +257,26 @@ class PlanFactory {
 				if ( ! empty( $custom_plan_data ) ) {
 					return new Plan( $custom_plan_data );
 				}
+				// fall-through to default case if no data was provided
 			case 'ecommerce':
 				if ( ! class_exists( 'NewfoldLabs\WP\Module\NextSteps\Data\Plans\StorePlan' ) ) {
 					require_once NFD_NEXTSTEPS_DIR . 'includes/Data/Plans/StorePlan.php';
 				}
 				return StorePlan::get_plan();
+				break;
 			case 'corporate':
 				if ( ! class_exists( 'NewfoldLabs\WP\Module\NextSteps\Data\Plans\CorporatePlan' ) ) {
 					require_once NFD_NEXTSTEPS_DIR . 'includes/Data/Plans/CorporatePlan.php';
 				}
 				return CorporatePlan::get_plan();
+				break;
 			case 'blog':
 			default:
 				if ( ! class_exists( 'NewfoldLabs\WP\Module\NextSteps\Data\Plans\BlogPlan' ) ) {
 					require_once NFD_NEXTSTEPS_DIR . 'includes/Data/Plans/BlogPlan.php';
 				}
 				return BlogPlan::get_plan();
+				break;
 		}
 	}
 
