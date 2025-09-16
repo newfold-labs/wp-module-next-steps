872f18c698dd80bad5e661d3b4e6577b797b8fd3
Evan Mullins <evanrm@gmail.com>
2025-09-16 12:47:09 -0400

    setup phpunit scaffolding as in other modules

---
+++ tests/wpunit/ExampleWPUnitTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace NewfoldLabs\WP\Module\Marketplace;
+
+/**
+ *  Example Unit test.
+ *
+ * @coversDefaultClass \NewfoldLabs\WP\Module\Marketplace\Example
+ */
+class ExampleWPUnitTest extends \lucatume\WPBrowser\TestCase\WPTestCase {
+
+}
