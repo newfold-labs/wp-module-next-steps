198efe9ac3fa34930f9c9b54cebdfc14a6eb88d0
Evan Mullins <evanrm@gmail.com>
2025-10-02 09:29:18 -0400

    update build files

--- build/next-steps-widget/bundle.asset.php
+++ build/next-steps-widget/bundle.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '8e65ec80cb29c849cb9a');
+<?php return array('dependencies' => array('lodash', 'react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '3b1ab6150df71bf53771');
