3be847fa5bda001ad05fea8fb0e8bcc51a6b789a
Evan Mullins <evanrm@gmail.com>
2025-10-06 10:41:59 -0400

    fresh build files to hopefully fix wireframe image issue

--- build/next-steps-portal/bundle.asset.php
+++ build/next-steps-portal/bundle.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-compose', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'f33249a54e6283d3be69');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-compose', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '27511728b470798ac6ef');
